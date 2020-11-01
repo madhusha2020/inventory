7b01762c6af487433d8ff66d171585b7fcfdb76f
malindu-hdps <malindu@interblocks.com>
2020-11-02 04:56:01 +0530

    Registration Service

---
+++ src/main/java/com/watersolution/inventory/component/entity/user/model/api/CustomerUser.java
@@ -0,0 +1,18 @@
+package com.watersolution.inventory.component.entity.user.model.api;
+
+import com.watersolution.inventory.component.common.exception.ResponseDefault;
+import com.watersolution.inventory.component.entity.customer.model.db.Customer;
+import com.watersolution.inventory.component.entity.user.model.db.User;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.Valid;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class CustomerUser extends ResponseDefault {
+    @Valid
+    private User user;
+    @Valid
+    private Customer customer;
+}
